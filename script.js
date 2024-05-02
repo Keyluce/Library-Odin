a14e30887e662eaf79d2ef9d8800d3240cd09377
Keyluce <86408122+Keyluce@users.noreply.github.com>
2024-05-02 20:50:30 +0530

    Add creation of new books through form functionality

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 const myLibrary = [];
 
-function Book(title, author, pages)
-{
-   this.title = title;
-   this.author = author;
-   this.pages = pages;
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
 }
 
-function addBookToMyLibrary(){
+function addBookToMyLibrary() {
     let title = prompt("Please enter the title of the book");
     let author = prompt("Please enter the author's name");
     let pages = prompt("Please enter the number of pages of the book");
-    myLibrary.push(new Book(title,author,pages));
+    myLibrary.push(new Book(title, author, pages));
 }
 
 
 let a = new Book("Harry Potter", "Hi", 10);
 let b = new Book("The Tree", "James", 9);
 let c = new Book("Superman", "Marsh", 50);
-myLibrary.push(a,b,c);
+myLibrary.push(a, b, c);
 
 
 const bookContainer = document.querySelector(".book-container");
@@ -27,8 +26,8 @@ myLibrary.forEach(function (book) {
     let a = document.createElement("div");
     a.classList.add("card");
     let b = document.createElement("h1");
-    b.textContent = book.title
-    ;
+    b.textContent = book.title;
+
     c = document.createElement("h2");
     c.textContent = `By ${book.author}`;
     d = document.createElement("h3");
@@ -40,9 +39,35 @@ myLibrary.forEach(function (book) {
     a.appendChild(d);
 });
 
-    const button = document.querySelector("button");
-    const sidebar = document.querySelector(".sidebar");
-    button.addEventListener("click", () => {
-        sidebar.classList.toggle("hidden");
-    });
+const button = document.querySelector(".new-book-button");
+const sidebar = document.querySelector(".sidebar");
+const submit = document.querySelector(".sidebar button[type=submit]");
+submit.addEventListener("click", submitEvent);
+
+function submitEvent(event) {
+    event.preventDefault();
+    let title = document.querySelector("form input[id='title'");
+    console.log(title.value);
+    let author = document.querySelector("form input[id='author'");
+    console.log(author.value);
+    let pages = document.querySelector("form input[id='pages'");
+    console.log(pages.value);
+    let a = document.createElement("div");
+    a.classList.add("card");
+    let b = document.createElement("h1");
+    b.textContent = title.value;
+    c = document.createElement("h2");
+    c.textContent = `By ${author.value}`;
+    d = document.createElement("h3");
+    d.textContent = pages.value + " pages";
+    bookContainer.appendChild(a);
+
+    a.appendChild(b);
+    a.appendChild(c);
+    a.appendChild(d);
+
+}
+button.addEventListener("click", () => {
+    sidebar.classList.toggle("hidden");
+});
 
