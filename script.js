90be0f7309f18952d34c57894cbf125c398c1636
Keyluce <86408122+Keyluce@users.noreply.github.com>
2024-05-03 20:10:55 +0530

    Add read button functionality to store whether book has been read or not

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.read = false;
 }
 
 function addBookToMyLibrary() {
@@ -12,48 +13,6 @@ function addBookToMyLibrary() {
     let pages = prompt("Please enter the number of pages of the book");
     myLibrary.push(new Book(title, author, pages));
 }
-
-
-let a = new Book("Harry Potter", "Hi", 10);
-let b = new Book("The Tree", "James", 9);
-let c = new Book("Superman", "Marsh", 50);
-myLibrary.push(a, b, c);
-
-
-const bookContainer = document.querySelector(".book-container");
-
-myLibrary.forEach(function (book,index) {
-    let a = document.createElement("div");
-    a.classList.add("card");
-    a.setAttribute("index", index);
-    let b = document.createElement("h1");
-    b.textContent = book.title;
-
-    c = document.createElement("h2");
-    c.textContent = `By ${book.author}`;
-    d = document.createElement("h3");
-    d.textContent = book.pages + " pages";
-    bookContainer.appendChild(a);
-  
-    a.appendChild(b);
-    a.appendChild(c);
-    a.appendChild(d);
-    let e = document.createElement("button");
-    e.classList.add("delete");
-    e.textContent = "Delete";
-    a.appendChild(e);
-});
-
-const button = document.querySelector(".new-book-button");
-const sidebar = document.querySelector(".sidebar");
-const submit = document.querySelector(".sidebar button[type=submit]");
-submit.addEventListener("click", submitEvent);
-const deleteButtons = document.querySelectorAll(".delete");
-deleteButtons.forEach(function(button)
-{
-    button.addEventListener("click", deleteBook);
-});
-
 function deleteBook(event)
 {
     let parent = event.target.parentElement;
@@ -71,7 +30,16 @@ function deleteBook(event)
         toAdjust.setAttribute("index", i-1);
     }
 
+}
 
+function readButton(event)
+{
+    let button = event.target;
+    let parent = button.parentElement;
+    console.log(button);
+    button.classList.toggle("green");
+    let indexBox = parent.getAttribute(`index`);
+    myLibrary[indexBox].read = button.classList.contains("green") ? true : false; 
 }
 function submitEvent(event) {
     event.preventDefault();
@@ -108,10 +76,71 @@ function submitEvent(event) {
     e.textContent = "Delete";
     e.addEventListener("click", deleteBook);
     a.appendChild(e);
+    let f = document.createElement("button");
+    f.classList.add("read");
+    f.textContent = "Read";
+    f.addEventListener("click", readButton);
+    a.appendChild(f);
     
 
 }
+
+
+let a = new Book("Harry Potter", "Hi", 10);
+let b = new Book("The Tree", "James", 9);
+let c = new Book("Superman", "Marsh", 50);
+myLibrary.push(a, b, c);
+
+
+const bookContainer = document.querySelector(".book-container");
+
+myLibrary.forEach(function (book,index) {
+    let a = document.createElement("div");
+    a.classList.add("card");
+    a.setAttribute("index", index);
+    let b = document.createElement("h1");
+    b.textContent = book.title;
+
+    c = document.createElement("h2");
+    c.textContent = `By ${book.author}`;
+    d = document.createElement("h3");
+    d.textContent = book.pages + " pages";
+    bookContainer.appendChild(a);
+  
+    a.appendChild(b);
+    a.appendChild(c);
+    a.appendChild(d);
+    let e = document.createElement("button");
+    e.classList.add("delete");
+    e.textContent = "Delete";
+    a.appendChild(e);
+    let f = document.createElement("button");
+    f.classList.add("read");
+    f.textContent = "Read";
+    f.addEventListener("click", readButton);
+    a.appendChild(f);
+});
+
+
+
+
+const button = document.querySelector(".new-book-button");
+const sidebar = document.querySelector(".sidebar");
+const submit = document.querySelector(".sidebar button[type=submit]");
+submit.addEventListener("click", submitEvent);
+const deleteButtons = document.querySelectorAll(".delete");
+
 button.addEventListener("click", () => {
     sidebar.classList.toggle("hidden");
 });
+
+
+
+deleteButtons.forEach(function(button)
+{
+    button.addEventListener("click", deleteBook);
+});
+
+
+
 
